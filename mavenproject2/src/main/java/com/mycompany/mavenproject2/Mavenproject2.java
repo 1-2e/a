50ba9767d92cede7d301f0c28e1a44369bd7e6ba
14237077 <14237077@Terra02.Fortis.local>
2024-06-03 11:43:59 +0200

    vv

--- mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
+++ mavenproject2/src/main/java/com/mycompany/mavenproject2/Mavenproject2.java
@@ -11,6 +11,6 @@ package com.mycompany.mavenproject2;
 public class Mavenproject2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Worlsndmjnsdnd!");
     }
 }
